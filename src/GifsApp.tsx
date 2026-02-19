2d6e2f2e59e6e3f7ff8c2b110b30263581e6c247
Adrian Fayos <adrianfayos1234@gmail.com>
2026-02-19 20:13:04 -0300

    http call by api key

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
+
 import { GifList } from './gifs/components/GifList'
 import { PreviousSearches } from './gifs/components/PreviousSearches'
 import { mockGifs } from './mock-data/gifs.mock'
 import { CustomHeader } from './shared/components/CustomHeader'
 import { SearchBar } from './shared/components/SearchBar'
+
+import { getGifsByQuery } from './gifs/actions/get-gifs-by-query.action'
 
 export const GifsApp = () => {
   const [previousTerms, setPreviousTerms] = useState(['dragon ball z'])
@@ -12,11 +15,15 @@ export const GifsApp = () => {
     console.log({ term })
   }
 
-  const handleSearch = (query: string) => {
+  const handleSearch = async (query: string) => {
     query = query.toLowerCase().trim()
     if (query.length === 0) return
     if (previousTerms.includes(query)) return
     setPreviousTerms([query, ...previousTerms].slice(0, 8))
+
+    const gifs = await getGifsByQuery(query)
+
+    console.log({ gifs })
   }
 
   return (
