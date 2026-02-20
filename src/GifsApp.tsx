882461c03ffc58c1ba779759ae8f772f777ef368
Adrian Fayos <adrianfayos1234@gmail.com>
2026-02-20 12:53:22 -0300

    show gifs from giphy

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 
 import { GifList } from './gifs/components/GifList'
 import { PreviousSearches } from './gifs/components/PreviousSearches'
-import { mockGifs } from './mock-data/gifs.mock'
+
 import { CustomHeader } from './shared/components/CustomHeader'
 import { SearchBar } from './shared/components/SearchBar'
 
 import { getGifsByQuery } from './gifs/actions/get-gifs-by-query.action'
+import type { Gif } from './gifs/interfaces/gif.interface'
 
 export const GifsApp = () => {
-  const [previousTerms, setPreviousTerms] = useState(['dragon ball z'])
+  const [gifs, setGifs] = useState<Gif[]>([])
+  const [previousTerms, setPreviousTerms] = useState<string[]>([])
 
   const handleTermClicked = (term: string) => {
     console.log({ term })
@@ -23,7 +25,7 @@ export const GifsApp = () => {
 
     const gifs = await getGifsByQuery(query)
 
-    console.log({ gifs })
+    setGifs(gifs)
   }
 
   return (
@@ -44,7 +46,7 @@ export const GifsApp = () => {
       />
 
       {/* Gifs */}
-      <GifList gifs={mockGifs} />
+      <GifList gifs={gifs} />
     </>
   )
 }
