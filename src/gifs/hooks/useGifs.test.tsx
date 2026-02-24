c960e3038321bfc4a7d6c15f04a39ead0c123526
Adrian Fayos <adrianfayos1234@gmail.com>
2026-02-24 19:47:53 -0300

    useGifs tests

---
+++ src/gifs/hooks/useGifs.test.tsx
@@ -0,0 +1,35 @@
+import { renderHook } from '@testing-library/react'
+import { describe, expect, test } from 'vitest'
+import { useGifs } from './useGifs'
+import { act } from 'react'
+
+describe('useGifs', () => {
+  test('should return default values and methods', () => {
+    const { result } = renderHook(() => useGifs())
+
+    expect(result.current.gifs.length).toBe(0)
+    expect(result.current.previousTerms.length).toBe(0)
+    expect(result.current.handleSearch).toBeDefined()
+    expect(result.current.handleTermClicked).toBeDefined()
+  })
+
+  test('should return a list of gifs', async () => {
+    const { result } = renderHook(() => useGifs())
+
+    await act(async () => {
+      await result.current.handleSearch('goku')
+    })
+
+    expect(result.current.gifs.length).toBe(10)
+  })
+
+  test('should return a list of gifs when handleTermClicked is called', async () => {
+    const { result } = renderHook(() => useGifs())
+
+    await act(async () => {
+      await result.current.handleTermClicked('goku')
+    })
+
+    expect(result.current.gifs.length).toBe(10)
+  })
+})
