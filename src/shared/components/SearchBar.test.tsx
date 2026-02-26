9405a9295340c7a404b845b07edd424d059b10ba
Adrian Fayos <adrianfayos1234@gmail.com>
2026-02-26 18:16:16 -0300

    more SearchBar tests and coverage

--- src/shared/components/SearchBar.test.tsx
+++ src/shared/components/SearchBar.test.tsx
@@ -27,4 +27,44 @@ describe('SearchBar', () => {
       { timeout: 3100 },
     )
   })
+
+  test('should call only once with the last value (debounce)', async () => {
+    const onQuery = vi.fn()
+    render(<SearchBar onQuery={onQuery} />)
+
+    const input = screen.getByRole('textbox')
+    fireEvent.change(input, { target: { value: 't' } })
+    fireEvent.change(input, { target: { value: 'te' } })
+    fireEvent.change(input, { target: { value: 'tes' } })
+    fireEvent.change(input, { target: { value: 'test' } })
+
+    await waitFor(
+      () => {
+        expect(onQuery).toHaveBeenCalledTimes(1)
+        expect(onQuery).toHaveBeenCalledWith('test')
+      },
+      { timeout: 3100 },
+    )
+  })
+
+  test('should call onQuery when button clicked with the input value', () => {
+    const onQuery = vi.fn()
+    render(<SearchBar onQuery={onQuery} />)
+
+    const input = screen.getByRole('textbox')
+    fireEvent.change(input, { target: { value: 'test' } })
+
+    const button = screen.getByRole('button')
+    fireEvent.click(button)
+
+    expect(onQuery).toHaveBeenCalledTimes(1)
+    expect(onQuery).toHaveBeenCalledWith('test')
+  })
+
+  test('shoul the input has the correct placeholder value', () => {
+    const value = 'Buscar gif'
+    render(<SearchBar onQuery={() => {}} placeholder={value} />)
+
+    expect(screen.getByPlaceholderText(value)).toBeDefined()
+  })
 })
